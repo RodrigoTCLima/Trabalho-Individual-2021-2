5ce22b2247203fbae6521120b3acdd03d2811a8a
Rodrigo Lima <rodrigotiagocl@gmail.com>
2022-05-02 01:53:50 -0300

    Correção em Dockerfile e entrypoint

--- aplicacao/entrypoint.sh
+++ aplicacao/entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
 rails db:create
 rails db:migrate
 rails db:seed
